28322c7c5dc8a5e97f1f9f9f0fc44e7458451f6f
Byron Omondi <omondibyron25@gmail.com>
2023-05-16 14:14:24 +0300

    complete assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements = []
+    for num in num_list:
+        if num % 2 == 0:
+            even_elements.append(num)
+    return even_elements
+    
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+    
